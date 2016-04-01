4ddf8daf1d741678bb994cb1ba5dbfb6e2b3833b
Dmitry <df.creative@gmail.com>
2016-03-31 23:29:31 -0400

    Add test with proper path resolving

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var execSync = require('child_process').execSync;
 var moduleBin = require('module-bin');
 var path = require('path');
+var callsite = require('callsite');
 
 function glslifySync(filePath) {
     var glslifyBinPath = moduleBin('glslify');
     var glslifyBinAbsolutePath = path.resolve('/', glslifyBinPath);
+
+    var stack = callsite();
+    var caller = stack[1].getFileName();
+
+    filePath = path.resolve(path.dirname(caller), filePath)
+
+    console.log(filePath);
+
     return execSync(glslifyBinAbsolutePath + ' ' + filePath, { encoding: 'utf8' });
 }
 
---
+++ test/index.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var glslify = require('../');
+var fs = require('fs');
+var path = require('path');
+var clean = require('cln');
+
+var fixture = fs.readFileSync(path.resolve(__dirname, './shader.glsl'), 'utf-8');
+
+
+var result = glslify('./shader.glsl');
+result = result.replace(/#[^\n]*/, '');
+
+assert.equal(clean(result), clean(fixture));
